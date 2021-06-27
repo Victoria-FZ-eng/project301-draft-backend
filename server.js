3d0594cf3cdf64a91eca3e785b7858328ac44154
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-27 12:49:58 +0300

    registeration

--- server.js
+++ server.js
@@ -10,13 +10,45 @@ app.use(express.json());
 const PORT = process.env.PORT || 3010;
 
 
-
+var users
 
 // localhost:3010/
 app.get('/', home);
 
 // localhost:3003/books?email=vzulof@gmail.com
 // app.get('/profile', goToProfile);
+
+//localhost:3010/newUser
+app.post('/newUser',usersInfo);
+
+function usersInfo(req,res){
+
+    console.log("inside saving function");
+    // try{
+    const {name, email, height, weight, age, favSport}= req.body;
+    // console.log(`adding user ${name, email, height, weight, favSport, age}`);
+
+    
+    const newUser= {
+        name: name,
+        email: email,
+        age:Number(age),
+        height: Number(height), 
+        weight: Number(weight),
+        favSport:favSport
+    };
+    users.push(newUser);
+    console.log(newUser);
+    
+    res.status(200).send(users);
+// }
+    // catch (err => {
+    //     res.status(500).send(`${err}: MOVIE'S DATA NOT FOUND FOR REQUIRED LOCATION`);
+    //     console.log("catch");
+    // });
+    console.log("pass");
+
+}
 
 function home (req, res){
     res.send('Home in Project 301');
